c302a986d2c9462a62cb48d1ffdc0708aac0b356
Yasas1994 <yasas.wijesekara@gmail.com>
2023-08-07 15:31:30 +0000

    partially working pipeline

--- workflow/scripts/split_multifasta.py
+++ workflow/scripts/split_multifasta.py
@@ -1,44 +1,61 @@
 #!/usr/bin/env python3
-import os
+import sys
 
-from Bio import SeqIO
+def split_multi_fasta(input_file, output_paths):
+    """
+    Splits a multi FASTA file into separate files and saves each sequence into specified output paths.
 
+    Parameters:
+        input_file (str): Path to the input multi FASTA file.
+        output_paths (list): List of output file paths where individual sequences will be saved.
 
-def split(fastafile="test_fasta.fasta", outfastadir="splitoutput"):
-    """Extract multiple sequence fasta file and write each sequence in separate file"""
-    os.system("mkdir -p %s" % (outfastadir))
-    with open(fastafile) as FH:
-        record = SeqIO.parse(FH, "fasta")
-        file_count = 0
-        for seq_rec in record:
-            file_count = file_count + 1
-            header = seq_rec.id
-            with open("%s/%s.fasta" % (outfastadir, str(header)), "w") as FHO:
-                SeqIO.write(seq_rec, FHO, "fasta")
-    if file_count == 0:
-        raise Exception("No valid sequence in fasta file")
-    return "Done"
+    Returns:
+        None
+    """
+
+    def write_fasta_record(output_path, header, sequence):
+        with open(output_path, 'a') as output_file:
+            output_file.write(f'{header}\n{sequence}\n')
+
+    current_output_path_index = 0
+    current_output_path = output_paths[current_output_path_index]
+
+    with open(input_file, 'r') as input_file:
+        header = ''
+        sequence = ''
+
+        for line in input_file:
+            line = line.strip()
+
+            if line.startswith('>'):
+                if header:
+                    write_fasta_record(current_output_path, header, sequence)
+                    sequence = ''
+
+                header = line
+                current_output_path = output_paths[current_output_path_index]
+                current_output_path_index = (current_output_path_index + 1) % len(output_paths)
+
+            else:
+                sequence += line
+
+        # Write the last sequence after the loop ends
+        if header:
+            write_fasta_record(current_output_path, header, sequence)
 
 
 if __name__ == "__main__":
-    import argparse
+    # Check if the correct number of arguments are provided
+    if len(sys.argv) < 3:
+        print("Usage: python script_name.py input_file output_path1 output_path2 ...")
+        sys.exit(1)
 
-    parser = argparse.ArgumentParser()
+    # First argument is the input file path
+    input_fasta_file = sys.argv[1]
 
-    parser.add_argument(
-        "-f",
-        "--fastafile",
-        action="store",
-        default="test_fasta.fasta",
-        help="Fasta File for parsing",
-    )
-    parser.add_argument(
-        "-d",
-        "--outfastadir",
-        action="store",
-        default="splitoutput",
-        help="Fasta File output directory",
-    )
+    # Remaining arguments are the output file paths
+    output_paths_list = sys.argv[2:]
 
-    args = parser.parse_args()
-    split(fastafile=args.fastafile, outfastadir=args.outfastadir)
+    # Call the function to split the multi FASTA file
+    split_multi_fasta(input_fasta_file, output_paths_list)
+
